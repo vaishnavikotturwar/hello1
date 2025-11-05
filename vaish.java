e92edfe869745dd651c1989222d40f9a0628d0d4
vaishnavikotturwar <vaishnavikotturwar5@gmail.com>
2025-11-05 18:58:39 +0530

    hii is added

---
+++ vaish.java
@@ -0,0 +1,11 @@
+class hello{
+public static void main(String args[]){
+int a=10,b=20,temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println("After Swapping");
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
+}
